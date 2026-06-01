f37e2c72ab3687b56d2b2f4551088d7c2b72663a
Irakli Mestvirishvili <imestvirishvili@credo.ge>
2026-06-01 18:05:39 +0400

    [ირაკლი მ.] HW5 — MultiplicationTable

---
+++ src/main/java/homework5/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package homework5;
+
+public class MultiplicationTable {
+    public static void printTable(int n) {
+        for(int i=1; i<=10; i++){
+            System.out.println(n + " * " + i + " = " + n*i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        printTable(7);
+    }
+}
