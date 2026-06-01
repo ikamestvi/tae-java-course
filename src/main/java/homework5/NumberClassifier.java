b504cced43b7a1bfa482e563bacf8c68706f3bbd
Irakli Mestvirishvili <imestvirishvili@credo.ge>
2026-06-01 17:53:07 +0400

    [ირაკლი მ.] HW5 — NumberClassifier

---
+++ src/main/java/homework5/NumberClassifier.java
@@ -0,0 +1,37 @@
+package homework5;
+
+public class NumberClassifier {
+    /*
+    დაწერე ორი overloaded მეთოდი:
+    String classify(double n) — იგივე + ცალკე check:
+    if ფრაქციული ნაწილია 0 → „Whole double", სხვა შემთხვევაში „Decimal"
+
+     */
+
+    public static String classify(int n) {
+        if(n>0){
+            return "Positive";
+        } else if (n<0) {
+            return "Negative";
+        }else return "Zero";
+    }
+
+
+    public static String classify(double n) {
+        if(n%2==0){
+            return "Whole double";
+        } else if (n%2!=0) {
+            return "Decimal";
+        }else return "Invalid";
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(classify(56));
+        System.out.println(classify(-45));
+        System.out.println(classify(0));
+        System.out.println(classify(4.00));
+        System.out.println(classify(9.15));
+    }
+}
