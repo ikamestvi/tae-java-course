a122edcd1a489d08b97d5bd29b9dfa1dce930c6b
Irakli Mestvirishvili <imestvirishvili@credo.ge>
2026-06-04 18:52:32 +0400

    [ირაკლი მ.] HW6 - BankAccount

---
+++ src/main/java/homework6/BankAccount.java
@@ -0,0 +1,31 @@
+package homework6;
+
+public class BankAccount {
+    private String owner;
+    private double  balance;
+
+    public BankAccount(String owner, double balance){
+        this.owner=owner;
+        this.balance=balance;
+    }
+
+
+    public void deposit(double amount){
+        if(amount>0){
+            this.balance+=amount;
+        }else System.out.println("თანხა უნდა იყოს დადებითი");
+    }
+
+
+
+    public void withdraw(double amount){
+        if(balance<=amount){
+            System.out.println("არასაკმარისი თანხა");
+        }else balance-=amount;
+    }
+
+
+    public double getBalance(){
+        return  balance;
+    }
+}
