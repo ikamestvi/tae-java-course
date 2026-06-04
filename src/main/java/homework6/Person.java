073e0aa8e2e8e08bce1424e327f7479ebf28e65e
Irakli Mestvirishvili <imestvirishvili@credo.ge>
2026-06-04 18:31:44 +0400

    [ირაკლი მ.] HW6 - Person

---
+++ src/main/java/homework6/Person.java
@@ -0,0 +1,36 @@
+package homework6;
+
+public class Person {
+    private String name;
+    private int age;
+    private String email;
+
+
+    public Person(String name, int age, String email){
+        this.name=name;
+        this.age=age;
+        this.email=email;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+
+    public int getAge(){
+        return age;
+    }
+
+
+    public String getEmail(){
+        return email;
+    }
+
+
+
+    public void introduce(){
+        System.out.println("გამარჯობა, მე ვარ " + getName() + ", " + getAge() + " წლის.");
+    }
+
+}
