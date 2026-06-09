9c008f9c536a2bc16510ac5eec8ef3d7d93e2462
Irakli Mestvirishvili <imestvirishvili@credo.ge>
2026-06-09 21:44:14 +0400

    [ირაკლი მ.] HW7 - Animal abstract

---
+++ src/main/java/homework7/Cat.java
@@ -0,0 +1,13 @@
+package homework7;
+
+public class Cat extends Animal{
+    public Cat(String name){
+        super(name);
+    }
+
+
+    @Override
+    public void makeSound(){
+        System.out.println("Meow!");
+    }
+}
