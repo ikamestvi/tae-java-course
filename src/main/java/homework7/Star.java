7a056aff838f9ecf4bc945c46caa281dbfd00549
Irakli Mestvirishvili <imestvirishvili@credo.ge>
2026-06-09 22:45:08 +0400

    [ირაკლი მ.] HW7 - Drawable polymorphism

---
+++ src/main/java/homework7/Star.java
@@ -0,0 +1,8 @@
+package homework7;
+
+public class Star implements Drawable{
+    @Override
+    public void draw() {
+        System.out.println("Drawing Star ★");
+    }
+}
---
+++ src/main/java/homework7/Triangle.java
@@ -0,0 +1,8 @@
+package homework7;
+
+public class Triangle implements Drawable{
+    @Override
+    public void draw() {
+        System.out.println("Drawing Triangle");
+    }
+}
